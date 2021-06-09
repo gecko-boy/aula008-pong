253143cac4954b48098594868108a4898962da13
gecko-boy <innergecko27@gmail.com>
2021-06-10 00:39:11 +0100

    Done

--- js/ball.js
+++ js/ball.js
@@ -1,5 +1,5 @@
 export default class Ball extends Phaser.GameObjects.Sprite {
-    constructor(scene, x, y, texture) {
+    constructor(scene, x, y, texture, paddleL, paddleR) {
         super(scene, x, y, texture);
 
         this.scene = scene;
@@ -11,14 +11,88 @@ export default class Ball extends Phaser.GameObjects.Sprite {
         );
 
         this.direction = {
-            x: 1,
-            y: 0
+            x: -1 + Math.random() * 2,
+            y: -1 + Math.random() * 2
         }
 
         this.speed = 2;
+        this.accel = 0.01;
+        this.speedLimit = 20;
+
+        this.paddleL = paddleL;
+        this.paddleR = paddleR;
+
+        this.halfSize = this.displayHeight * 0.5;
+    }
+
+    setPaddles(paddleL, paddleR) {
+        this.paddleL = paddleL;
+        this.paddleR = paddleR;
+    }
+
+    moveBall() {
+        if(this.speed < this.speedLimit) {
+            this.speed += this.accel;
+        }
+        this.x += this.speed * this.direction.x;
+        this.y += this.speed * this.direction.y;
+    }
+
+    checkCollisions() {
+        let myBounds = this.getBounds();
+        let boundsL = this.paddleL.getBounds();
+        let boundsR = this.paddleR.getBounds();
+
+        if(Phaser.Geom.Intersects.RectangleToRectangle(myBounds, boundsL) || Phaser.Geom.Intersects.RectangleToRectangle(myBounds, boundsR)) {
+            this.reverseMe("x");
+        }
+
+        if(this.y <= this.halfSize || this.y >= this.scene.game.config.height - this.halfSize) {
+            this.reverseMe("y");
+        }
+    }
+
+    checkScoreAndReset() {
+        if(this.x <= 0) {
+            this.scene.scoreKeeper("right");
+            this.resetMe();
+        }
+
+        if(this.x >= this.scene.game.config.width) {
+            this.scene.scoreKeeper("left");
+            this.resetMe();
+        }
+    }
+
+    reverseMe(axis) {
+        // if(axis == "x") {
+        //     this.direction.x = -this.direction.x;
+        // }
+        // if(axis == "y") {
+        //     this.direction.y = -this.direction.y;
+        // }
+
+        this.direction[axis] = -this.direction[axis];
+    }
+
+    resetMe() {
+        this.setPosition(
+            this.scene.game.config.width * 0.5,
+            this.scene.game.config.height * 0.5
+        );
+
+        this.direction = {
+            x: -1 + Math.random() * 2,
+            y: -1 + Math.random() * 2
+        }
+
+        this.speed = 2;
+
     }
 
     update(time) {
-        
+        this.checkCollisions();
+        this.moveBall();
+        this.checkScoreAndReset();
     }
 }
--- js/scene.js
+++ js/scene.js
@@ -8,6 +8,11 @@ export default class MainScene extends Phaser.Scene {
 
     init() {
         this.paddleOffsetX = 80;
+
+        this.score = {
+            left: 0,
+            right: 0
+        }
     }
 
     preload() {
@@ -16,7 +21,7 @@ export default class MainScene extends Phaser.Scene {
     }
 
     create() {
-        this.paddleL = this.add.existing(new Paddle(this, this.paddleOffsetX, this.game.config.height * 0.5, "paddle", {"up": 87, "down": 83}));
+        this.paddleL = this.add.existing(new Paddle(this, this.paddleOffsetX, this.game.config.height * 0.5, "paddle", {"up": 65, "down": 90}));
 
         this.paddleL.init();
 
@@ -24,13 +29,34 @@ export default class MainScene extends Phaser.Scene {
 
         this.paddleR.init();
 
-        this.ball = this.add.existing(new Ball(this, 0, 0, "ball"));
-        
+        this.ball = this.add.existing(new Ball(this, 0, 0, "ball", this.paddleL, this.paddleR));
+
+        this.scoreText = this.add.text(
+            this.game.config.width * 0.5, 80, `${this.score.left} - ${this.score.right}`, 
+            {
+                fontFamily: 'Arial',
+                fontSize: 64,
+                color: '#fff',
+                align: 'center'
+            }
+        ).setOrigin(0.5)
+    }
+
+    scoreKeeper(paddle) {
+        // if(paddle == "left") {
+        //     this.score.left += 1;
+        // } else if (paddle == "right") {
+        //     this.score.right += 1;
+        // }
+
+        this.score[paddle] += 1;
     }
 
     update(time) {
         this.paddleL.update(time);
         this.paddleR.update(time);
         this.ball.update(time);
+
+        this.scoreText.text = `${this.score.left} - ${this.score.right}`;
     }
 }
